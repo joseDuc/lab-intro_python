c29c54454ca0f154d5600655e844d8de91a64611
joseDuc <ppgq@live.com>
2024-07-25 18:29:02 +0200

    second lab-intro_python

--- index.py
+++ index.py
@@ -1,8 +1,7 @@
+
 from flask import Flask, request, jsonify, render_template
 import json
 import os
-
-terrario=''
 
 app = Flask(__name__)
 
@@ -14,58 +13,73 @@ def index():
 def enviar():
     try:
         data=request.get_json()
-        nombre = data.get('name', '')
-        medida=data.get('size', '')
-        especie=data.get('species','')
-        if not nombre or not medida or not especie:
-            return  jsonify({'mensaje' : f'Faltan datos para guardar al habitante'})
-        s=Serpiente(nombre,float(medida),especie)
+        name = data.get('name', '')
+        size=data.get('size', '')
+        species=data.get('species','')
+        if not name or not size or not species:
+            return  jsonify({'message' : f'Faltan datos para guardar al habitante'})
+        s=Snake(name,float(size),species)
         descargaHabitante(s)
-        return jsonify({'mensaje' : f'Se ha agregado la serpiente {s.nombre} de {s.medida} mts de la especie {s.especie}'})
+        return jsonify({'message' : f'Se ha agregado la serpiente {s.name} de {s.size} mts de la especie {s.species}'})
     except Exception as e:
-        return jsonify({'mensaje': f"Error: {str(e)}"}), 500
+        return jsonify({'message': f"Error: {str(e)}"}), 500
 
-@app.route('/listar',methods=['GET'])
+@app.route('/listar', methods=['GET'])
 def listar():
-    return jsonify({'mensaje' : cargaHabitantes()})
+    return jsonify({'message' : cargaHabitantes()})
 
-class Serpiente:
-    def __init__(self, nombre,medida,especie):
-        self.nombre=nombre
-        self.medida=medida
-        self.especie=especie
+@app.route('/cargaAnimales', methods=['GET'])
+def  cargaAnimales():
+    try:
+        animals=cargaHabitantes()
+        return jsonify({f'message': animals})
+    except Exception as e:
+        return jsonify({'message':  f"SSSerror: {str(e)}"}),500
+@app.route('/nombreTerrario',methods=['GET'])
 
-    def deslizar(self):
-        print (f"n/La serpiente {self.nombre} se desliza")
+
+def nombreTerrario():
+    return ({'message':'Terrario del África'})
+    
+    
+class Snake:
+    def __init__(self, name,size,species):
+        self.name=name
+        self.size=size
+        self.species=species
+
+    def move(self):
+        print (f"La serpiente {self.name} se desliza")
     
     
 class Terrario:
-    def __init__(self, nombre,ancho,largo):
-        self.nombre=nombre
-        self.ancho=ancho
-        self.largo=largo
-        self.habitantes=[]
+    def __init__(self, name, width, long):
+        self.name=name
+        self.width=width
+        self.long=long
+        self.animals=[0]
         
-    def agregaHabitante(self,serpiente):
+    def agregaHabitante(self,snake):
         try:
-            if not isinstance(serpiente,Serpiente):
-                print(f"n/El objeto {serpiente} no es una serpiente.")
+            if type(snake) is Snake:
+                self.animals.append(snake)
             else:
-                self.habitantes.append(serpiente)
-                return self.habitantes[self.habitantes.len-1]
+                print(f"El objeto {snake} no es una serpiente.")
+            return self.animals[self.animals.len-1]
         except Exception as e:
             return (f'error agregando error: {e}')
         
-        
-    def muestraHabitantes(self,):
-        if not self.habitantes:
-            print ("n/No hay habitantes")
-        else:
-            for h in self.habitantes:
-                print(f"n/nombre: {h.nombre} medida: {h.medida} especie: {h.especie}")
+
     
+    def muestraHabitantes(self):
+        if not self.animals:
+            print ("No hay habitantes")
+        else:
+            for s in self.animals:
+                print(f'name:{s.name} size:{s.size} species:{s.species}')
+
     def area(self):
-        return self.ancho*self.largo
+        return (float(self.width) * float(self.long))
     
 
    
@@ -75,42 +89,43 @@ def nombreDeArchivo():
 def cargaHabitantes():
     if os.path.exists(nombreDeArchivo()):
         with open(nombreDeArchivo(), 'r') as f:
-                habitantes= json.load(f)
-                return habitantes
+            animals= json.load(f)
+            return animals
     else:
         return []
             
 def descargaHabitantes(terrario):
     if terrario:
         with open(nombreDeArchivo(), 'w') as f:
-            json.dump(terrario.habitantes, f, indent=4)
+            json.dump(terrario.animals, f, indent=4)
    
-def descargaHabitante(serpiente):
-    if serpiente:
-        habitantes =cargaHabitantes()
-       # habitantes.append(serpiente)
-        habitantes.append({'nombre': serpiente.nombre, 'medida': serpiente.medida, 'especie': serpiente.especie})
+def descargaHabitante(snake):
+    if snake:
+        animals =cargaHabitantes()
+       # animals.append(serpiente)
+        animals.append({'name': snake.name, 'size': snake.size, 'species': snake.species})
             
         with open(nombreDeArchivo(), 'w') as f:
-            json.dump(habitantes, f, indent=4)
+            json.dump(animals, f, indent=4)
    
 
 def main():
     terrario = Terrario('África', 5.40, 7.60)
-
-    terrario.agregaHabitante(Serpiente('Mamba Negra',4.5,'Elápido'))
-    terrario.agregaHabitante(Serpiente('Pitón de Seba',6,'Pitónido'))
-    terrario.agregaHabitante(Serpiente('Serpiente de Árbol del Gabón',2,'Viperido'))
-    terrario.agregaHabitante(Serpiente('Cobra Egipcia',2.5,'Elápido'))
-    terrario.agregaHabitante(Serpiente('Serpiente de Arena del Sáhara',4.5,'Viperido'))
+    terrario.agregaHabitante(Snake('Mamba Negra',4.5,'Elápido'))
+    terrario.agregaHabitante(Snake('Pitón de Seba',6,'Pitónido'))
+    terrario.agregaHabitante(Snake('Serpiente de Árbol del Gabón',2,'Viperido'))
+    terrario.agregaHabitante(Snake('Cobra Egipcia',2.5,'Elápido'))
+    terrario.agregaHabitante(Snake('Serpiente de Arena del Sáhara',4.5,'Viperido'))
     
     terrario.muestraHabitantes()
+
     
-    print (f"n/Área del terrario: {terrario.area}")
+    print (f'Área del terrario: {terrario.area}')
     
-    habitantes = terrario.habitantes
-    for s in habitantes:
-        s.desliza()
+   
+
+    for a in terrario.animals:
+        a.move()
     
     h=cargaHabitantes()
     if h:
--- static/js/index.js
+++ static/js/index.js
@@ -1,49 +1,106 @@
 
 window.addEventListener("load", () => {
+  const f = document.querySelector("#form_insert_animal");
+  f.addEventListener('submit', enviaFormInsert);
   document.querySelector("#form_insert_animal").onsubmit = async function (event) {
-
-    event.preventDefault();
-
-    const name = document.querySelector("#text_name").value;
-    const size = document.querySelector("#text_size").value;
-    const species = document.querySelector("#text_species").value;
-
-    if (!name || !size || !species) {
-      alert("Faltan datos para guardar");
-      return;
-    }
-    try {
-      const response1 = await fetch("enviar", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name, size, species }),
-      });
-      const data1 = await response1.json();
-
-      if (data1) {
-        document.querySelector("#mensaje_register_response").innerText = data1.mensaje;
-      }
-      document.querySelector("#text_name").value = '';
-      document.querySelector("#text_size").value = '';
-      document.querySelector("#text_species").value = '';
-
-      const response2 = await fetch("listar", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const data2 = await response2.json();
-      alert (data2.mensaje[0]);
-
-    } catch (e) {
-      alert(e);
-    }
   };
+  cargaAnimales();
 });
 
-function enviaFormInsert(){
+async function enviaFormInsert(e) {
+  e.preventDefault();
+
+  document.querySelector("#mensaje_register_response").innerText = "";
+  const name = document.querySelector("#name").value;
+  const size = document.querySelector("#size").value;
+  const species = document.querySelector("#species").value;
+
+  if (!name || !size || !species) {
+    alert("Faltan datos para guardar");
+    return;
+  }
+
+  try {
+
+    const response = await fetch("enviar", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, size, species }),
+    });
+    const data = await response.json();
+
+    document.querySelector("#name").value = '';
+    document.querySelector("#size").value = '';
+    document.querySelector("#species").value = '';
+
+    if (data) {
+      document.querySelector("#mensaje_register_response").innerText = data.message;
+    }
+
+    cargaAnimales();
+  } catch (e) {
+    alert(e);
+  }
+
+}
+
+async function cargaAnimales() {
+  try {
+    const response = await fetch("cargaAnimales", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    installAnimals(data.message);
+
+  } catch (e) {
+    alert(e);
+  }
+}
+function installAnimals(animals) {
+  if (animals) {
+    const l = document.querySelector('#list_animal');
+    const c = l.querySelector('.container');
+    if (c) {
+      c.innerHTML = '';
+      const t = document.createElement('table');
+      const tr = document.createElement('tr');
+      const th = document.createElement('th');
+
+      let html = '<th>nombre</th>';
+      html += '<th>medida</th>';
+      html += '<th>especie</th>';
+      html += '<th>accion</th>';
+      tr.innerHTML = html
+      t.appendChild(tr);
+    console.log(animals)
+      html='<td>' + animals[0].name +'</td>';
+       html+='<td>' + animals[0].size +'</td>';
+        html+='<td>' + animals[0].species +'</td>';
+      const tr1=document.createElement('tr');
+      tr1.innerHTML=html;
+      t.appendChild(tr1);
+      c.appendChild(t);
+    }
+
+  }
+
+}
+function creaRowAnimal(animal) {
+  if (animal) {
+    const dr = document.createElement('tr');
+    let html  = '<td>' + $(animal.name) + '</td>';
+    html += '<td>' + $(animal.size) + '</td>';
+    html = '<td>' + $(animal.species) + '</td>';
+    dr.innerHTML = html;
+    return dr;
+  }else{
+    return null;
+  }
+ 
 
 }
